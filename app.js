95e83b679489638f79b7c31301568f448f7727c3
eqzhou81 <eqzhou81@gmail.com>
2023-03-08 00:57:03 -0800

    Fix dotenv config

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser()); 
+
+const userRoute = require('./routes/userRoute');
+const taskRoute = require('./routes/taskRoute');
+
+mongoose.connect("mongodb+srv://eqzhou81:PowerIt0308!@todolist-gql.vni9p8m.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connection.once("open", () => {
+    console.log("Connected to database");
+});
+
+const port = 3000;
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.use('/user', userRoute);
+
+app.use('/task', taskRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
---
+++ auth.js
@@ -0,0 +1,21 @@
+const userModel = require('./models/userModel');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// Check if user is logged into correct account to update/delete their tasks
+const isAuthenticated = async (req,res,next)=>{
+    try {
+        const token = req.cookies.token;
+        if(!token){
+            return next('Please login to access the data');
+        }
+        const verify = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = await userModel.findById(verify.userExist);
+        req.token = token;
+        next();
+    } catch (error) {
+       return next(error); 
+    }
+}
+
+module.exports = isAuthenticated;
